8406c6d969c51e0575bf087587891cf94c355d03
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-17 13:33:46 +0200

    Update megajs.js

--- megajs/megajs.js
+++ megajs/megajs.js
@@ -1,7 +1,7 @@
 var mega = require("megajs");
 const auth = {
     email: 'octa@onlyfans.com',
-    password: 'your mega pass word',
+    password: 'KL WHITE SER@123',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 

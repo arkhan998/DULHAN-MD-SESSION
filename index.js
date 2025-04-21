8df011c7a5c11aba5a2fed17c9b9a95194f53399
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-04-21 18:56:51 +0200

    naxor

--- index.js
+++ index.js
@@ -1,80 +1,129 @@
-const axios = require('axios');
-const { create } = require('./megajs/makeSeeion');
-const { toid } = require('./megajs/id');
 const express = require('express');
+const path = require('path');
+const body = require("body-parser");
+const axios = require('axios');
+const { create } = require('./megajs/makeSession');
+const { get } = require("./megajs/makeSession"); 
+
+function makeId(len = 4) {
+    let res = '';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < len; i++) {
+        res += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return res;
+}
+
 const fs = require('fs');
-let router = express.Router();
 const pino = require("pino");
 const {
-    default: makeWASocket,
+    default: WASocket,
     useMultiFileAuthState,
     delay,
     Browsers,
     makeCacheableSignalKeyStore
 } = require("@whiskeysockets/baileys");
 
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
-};
+const app = express();
+const root = process.cwd();
+const port = process.env.PORT || 5000;
 
-router.get('/', async (req, res) => {
-    const id = makeid();
+app.use(body.json());
+app.use(body.urlencoded({ extended: true }));
+app.use(express.static(path.join(root, 'statics')));
+
+function rmFile(path) {
+    if (!fs.existsSync(path)) return false;
+    fs.rmSync(path, { recursive: true, force: true });
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(root, '/statics/index.html'));
+});
+
+app.get('/pair.html', (req, res) => {
+    res.sendFile(path.join(root, '/statics/pair.html'));
+});
+
+app.get('/session', async (req, res) => {
+    const q = req.query.q;
+    if (!q) { return res.status(400).json({
+            ok: false,
+            msg: 'Query parameter "q" is required.'
+        });
+    }
+
+    const query = q.split(';')[1];
+    try { const data = await get(query);
+        res.json({
+            ok: true,
+            data: data.content
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            ok: false,
+            msg: 'Internal server error.',
+            err: err.message
+        });
+    }
+});
+
+app.get('/pair', async (req, res) => {
+    const id = makeId();
     let num = req.query.number;
-
-    async function getPaire() {
+    async function pair() {
         const { state, saveCreds } = await useMultiFileAuthState('./session/' + id);
         try {
-            let session = makeWASocket({
+            let wa = WASocket({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
-                printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: Browsers.macOS("Safari"),
-             });
+            });
 
-            if (!session.authState.creds.registered) {
+            if (!wa.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await session.requestPairingCode(num);
+                const code = await wa.requestPairingCode(num);
                 if (!res.headersSent) {
-                    await res.send({ code });
+                    await res.json({ code });
                 }
             }
 
-            session.ev.on('creds.update', saveCreds);
+            wa.ev.on('creds.update', saveCreds);
 
-            session.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
+            wa.ev.on("connection.update", async (state) => {
+                const { connection, lastDisconnect } = state;
 
                 if (connection == "open") {
                     await delay(5000);
                     await delay(5000);
-
-                    const jsonData = await fs.promises.readFile(`${__dirname}/session/${id}/creds.json`, 'utf-8');     
-                    const { id: data } = await create(jsonData);
-                    await session.sendMessage(session.user.id, { text: 'xastral~' + data });
-
+                    const json = await fs.promises.readFile(`${root}/session/${id}/creds.json`, 'utf-8');     
+                    const { id: sessionId } = await create(json);
+                    await wa.sendMessage(wa.user.id, { text: 'xastral~' + sessionId });
                     await delay(100);
-                    await session.ws.close();
-                    return await removeFile('./session/' + id);
+                    await wa.ws.close();
+                    return await rmFile('./session/' + id);
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    getPaire();
+                    pair();
                 }
             });
         } catch (err) {
-            console.log("service restated");
-            await removeFile('./session/' + id);
+            console.error(err);
+            await rmFile('./session/' + id);
             if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
+                res.status(500).json({ error: "Service Unavailable" });
             }
         }
     }
 
-    return await getPaire();
+    return await pair();
 });
 
-module.exports = router;
+app.listen(port, () => {
+    console.log(`Server running on:${port}`);
+});

5617aacdbf439ceafb0e2bed09a675ad094f9387
naxordeve <109889758+Diegoson@users.noreply.github.com>
2025-05-23 13:24:15 +0200

    img

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const {
     delay,
     Browsers,
     makeCacheableSignalKeyStore
-} = require("baileys");
+} = require("@whiskeysockets/baileys");
 
 var app = express();
 var root = process.cwd();
